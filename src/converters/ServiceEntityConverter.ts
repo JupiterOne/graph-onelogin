36b8645e559cd8305566042acd3bb9f23bfd7b40
Matthew Zember <matthew.zember@jupiterone.com>
2022-08-24 07:13:11 -0400

    run prettier

--- src/converters/ServiceEntityConverter.ts
+++ src/converters/ServiceEntityConverter.ts
@@ -2,7 +2,7 @@ import {
   SERVICE_ENTITY_CLASS,
   SERVICE_ENTITY_TYPE,
   ServiceEntity,
-} from "../jupiterone";
+} from '../jupiterone';
 
 export function getServiceEntities(
   oneloginAccountInfoName: string,
@@ -11,22 +11,22 @@ export function getServiceEntities(
     _type: SERVICE_ENTITY_TYPE,
     _key: `onelogin:sso:${oneloginAccountInfoName}`,
     _class: SERVICE_ENTITY_CLASS,
-    name: "SSO",
-    displayName: "Onelogin SSO",
-    category: "security",
-    function: "sso",
-    controlDomain: "identity-access",
+    name: 'SSO',
+    displayName: 'Onelogin SSO',
+    category: 'security',
+    function: 'sso',
+    controlDomain: 'identity-access',
   };
 
   const mfaService: ServiceEntity = {
     _type: SERVICE_ENTITY_TYPE,
     _key: `onelogin:mfa:${oneloginAccountInfoName}`,
     _class: SERVICE_ENTITY_CLASS,
-    name: "MFA",
-    displayName: "Onelogin MFA",
-    category: "security",
-    function: "mfa",
-    controlDomain: "identity-access",
+    name: 'MFA',
+    displayName: 'Onelogin MFA',
+    category: 'security',
+    function: 'mfa',
+    controlDomain: 'identity-access',
   };
 
   return [ssoService, mfaService];
